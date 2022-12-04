945397e2b9d62ee030b0bd2629336cce7fcd8fe8
Katherine Blaine <blaine.katherine12@gmail.com>
2022-12-03 18:47:56 -0600

    Add book functions for iteration 1

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(firstName, yearsOld, goesBy) {
+  return character = {
+    name: firstName,
+    age: yearsOld,
+    pronouns: goesBy
+  };
+};
+
+function saveReview(value, location) {
+  if (location.includes(value) === false) {
+  return location.push(value);
+  };
+};
+
+function calculatePageCount(title) {
+  return title.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, type) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: type
+  };
+};
+
+function editBook() {
+  book.pageCount *= (3/4);
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
