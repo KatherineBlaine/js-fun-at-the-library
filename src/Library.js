b9401c6aa9f9a766f2a2252ba5a37a93cc9d5dd3
Katherine Blaine <blaine.katherine12@gmail.com>
2022-12-04 20:48:55 -0600

    Add library functions for iteration 3

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,42 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  };
+  return library;
+}
+
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+  return library.shelves.fantasy.push(book);
+  } if (book.genre === 'nonFiction') {
+    return library.shelves.nonFiction.push(book);
+  } if (book.genre === 'fiction') {
+    return library.shelves.fiction.push(book);
+  }
+}
+
+
+function checkoutBook(library, book, genre) {
+  if (genre === 'fantasy') {
+    library.shelves.fantasy.splice(0, 1);
+  } else if (genre === 'nonFiction') {
+    library.shelves.nonFiction.splice(0, 1);
+  } else if (genre === 'fiction') {
+    library.shelves.fiction.splice(0, 1);
+  }
+  return `You have now checked out ${book} from the ${library.name}`;
+}
+  
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
